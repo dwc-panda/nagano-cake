fdd83e3369313ebc09c3fc75b9cb07d0b80a49ae
daihuku99 <dai9huku@gmail.com>
2020-04-18 21:56:44 +0900

    update end_users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,18 @@ class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource_or_scope)
 		if resource_or_scope == :user
 			'/'
-		else resource_or_scope == :end_user
+		elsif resource_or_scope == :end_user
 			items_path
+		else
+		end
+	end
+
+	def after_sign_out_path_for(resource_or_scope)
+		if resource_or_scope == :user
+			new_admins_user_sessoin_path
+		elsif resource_or_scope == :end_user
+			items_path
+		else
 		end
 	end
 end
