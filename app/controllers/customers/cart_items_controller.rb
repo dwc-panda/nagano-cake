7e62f3b5bfbad4612a428c5e9cacc99a83a5359d
unknown <grenson.crtis@gmail.com>
2020-04-19 11:36:35 +0900

    update

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -2,18 +2,18 @@ class Customers::CartItemsController < ApplicationController
 
 	def index
        @cart_items = CartItem.all
-       @items = @cart_item.item
-       @price_with_tax = cart_item.item.non_tax_price.to_i * 1.1
-       @subtotal = @price_with_tax * quantity
+       # @items = @cart_item.item
+       # @price_with_tax = cart_item.item.non_tax_price.to_i * 1.1
+       # @subtotal = @price_with_tax * quantity
 	end
 
 	def create
 		cart_item =CartItem.new(cart_items_params)
-		cart_item.end_user_id = current_user.id
+		cart_item.end_user_id = current_end_user.id
 		cart_item.item_id = item.id
-		if @cart_item.save
+		if cart_item.save
+		   redirect_back(fallback_location: root_path)
 		end
-		redirect_back(fallback_location: root_path)
 	end
 
 	end
@@ -40,6 +40,7 @@ class Customers::CartItemsController < ApplicationController
 
 	private
 	def cart_items_params
-		params.require(cart_item).permit(:end_user_id, :item_id, :quantity)
+		params.require(:cart_item).permit(:item_id, :quantity)
+	end
 
 end
