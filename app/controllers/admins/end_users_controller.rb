c0eb2b0275af96fa35d98b9901d06dde76c8edfd
383528 <tu2.25.11.30@gmail.com>
2020-04-19 12:09:53 +0900

    update admins_end_user_controller

--- app/controllers/admins/end_users_controller.rb
+++ app/controllers/admins/end_users_controller.rb
@@ -1,11 +1,13 @@
 class Admins::EndUsersController < ApplicationController
 
 def index
+  # @end_user = EndUser.find(params[:id])
   @end_users = EndUser.all
 end
 
 def show
-  @end_user = current_end_user
+   @end_user = current_end_user
+  # @end_user = EndUser.find(params[:id])
 end
 
 def edit
@@ -15,8 +17,11 @@ end
 
 def update
   @end_user = EndUser.find(params[:id])
-  @end_user.update(end_user_params)
+  if @end_user.update(end_user_params)
   redirect_to admins_end_user_path(@end_user)
+  else
+	redirect_back(fallback_location: root_path)
+  end
 end
 private
 
