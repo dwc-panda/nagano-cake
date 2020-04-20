c440b64dabbd6ab1d87c4a779092043ccf0c23e2
unknown <grenson.crtis@gmail.com>
2020-04-20 20:44:00 +0900

    addcontroller

---
+++ app/controllers/admins/orders_controller.rb
@@ -0,0 +1,10 @@
+class Admins::OrdersController < ApplicationController
+  def index
+  	@orders = Order.all
+  end
+
+  def show
+  	@order = Order.find(params[:id])
+  	@orders = Order.all
+  end
+end
