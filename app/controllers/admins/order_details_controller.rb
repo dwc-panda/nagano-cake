4451100eb0713a875242ec591c9bbdf201d391b6
unknown <grenson.crtis@gmail.com>
2020-04-22 11:45:18 +0900

    updateview_bootstrap

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -2,9 +2,8 @@ class Admins::OrderDetailsController < ApplicationController
 
 	def update
 		@order_detail = OrderDetail.find(params[:id])
-		@order_detail.item_status = params[:order_detail][:item_status].to_i
-
-		@order_detail.update(order_detail_params)
+		@order_detail.item_status = params[:order_detail][:item_status]
+		@order_detail.save
 		redirect_to admins_orders_path
 	end
 
