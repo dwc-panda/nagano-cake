56f73962d2ea8e228c1cea6b6e6db17f6835350e
383528 <tu2.25.11.30@gmail.com>
2020-04-22 11:44:41 +0900

    update order

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -1,11 +1,19 @@
 class Admins::OrderDetailsController < ApplicationController
 
-	def update
-		@order_detail = OrderDetail.find(params[:id])
-		@order_detail.item_status = params[:order_detail][:item_status].to_i
+	# def update
+	# 	@order_detail = OrderDetail.find(params[:id])
+	# 	@order_detail.item_status = params[:order_detail][:item_status].to_i
 
-		@order_detail.update(order_detail_params)
-		redirect_to admins_orders_path
+	# 	@order_detail.update(order_detail_params)
+	# 	redirect_to admins_orders_path
+	# end
+	# 梶谷さん修正後
+	 def update
+		@order_detail = OrderDetail.find(params[:id])
+		@order_detail.item_status = params[:order_detail][:item_status]
+		@order_detail.save
+		#挙動確認のためredirect_backに変更
+		redirect_back(fallback_location: root_path)
 	end
 
 	private
