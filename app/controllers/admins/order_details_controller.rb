7639987ab3cc40e52280226acd6fdfc4228700e2
daihuku99 <dai9huku@gmail.com>
2020-04-23 21:46:01 +0900

    update order_detail/controller

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -4,12 +4,14 @@ before_action :authenticate_admins_user!
 		@order_detail = OrderDetail.find(params[:id])
 		@order_detail.item_status = params[:order_detail][:item_status]
 		@order_detail.save
-		@array_status = @order_detail.order.order_details.pluck(:item_status)
-    	@array_status.delete("制作完了")
-		if @order_detail.item_status == "制作中"
-			@order_detail.order.order_status = "制作中"
+		# @array_status = @order_detail.order.order_details.pluck(:item_status)
+    #@array_status.delete("製作完了")
+    @a = @order_detail.order.order_details.pluck(:item_status)
+		if @order_detail.item_status == "製作中"
+			@order_detail.order.order_status = "製作中"
 			@order_detail.order.update(order_status: 2)
-		elsif @array_status.length == 0
+		# elsif @array_status.length == 0
+	  elsif @a.count("製作完了") == @order_detail.order.order_details.count
 			@order_detail.order.update(order_status: 3)
 		end
 		@order_detail.order
@@ -21,5 +23,7 @@ before_action :authenticate_admins_user!
 		params.require(:order_detail).permit(:item_status)
 	end
 end
+
+
 
 
